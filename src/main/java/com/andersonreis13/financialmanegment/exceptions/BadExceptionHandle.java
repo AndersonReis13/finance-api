2646a92592079743e22d824a049c612b1eafc6cd
anderson <andersonline62@gmail.com>
2025-09-05 09:19:12 -0300

    feat: Foi adicionado o tratamento de exceções global para o projeto.

---
+++ src/main/java/com/andersonreis13/financialmanegment/exceptions/BadExceptionHandle.java
@@ -0,0 +1,7 @@
+package com.andersonreis13.financialmanegment.exceptions;
+
+public class BadExceptionHandle extends RuntimeException {
+    public BadExceptionHandle(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/andersonreis13/financialmanegment/exceptions/handle/CustomizedExceptionHandle.java
@@ -0,0 +1,25 @@
+package com.andersonreis13.financialmanegment.exceptions.handle;
+
+import com.andersonreis13.financialmanegment.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class CustomizedExceptionHandle {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ModelFormExceptionHandle> NotFoundExceptionHandle(WebRequest request,
+                                                                           NotFoundException ex){
+        ModelFormExceptionHandle modelFormExceptionHandle = new ModelFormExceptionHandle(
+                ex.getMessage(),
+                request.getDescription(false),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND
+        );
+        return new ResponseEntity<>(modelFormExceptionHandle, HttpStatus.NOT_FOUND);
+    }
+
+}
